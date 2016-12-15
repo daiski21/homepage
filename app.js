c4fbe0ac3c06cbcd9d7f34cca2abfd3401f7fcd1
daiski21 <ampitmelving@gmail.com>
2016-12-15 19:40:56 +0800

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-var express = require('express')
-var app = express()
-var path = require('path')
+var express = require('express');
+var path = require('path');
+var app = express();
 
-app.use(express.static(path.join(__dirname,'public')))
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(path.join(__dirname, 'public')))
 
-// app.get('/', function (req, res) {
-//   res.send('<h2> Hello World!</h2>')
-// })
-// app.get('/Home', function (req, res) {
-//   res.sendFile('index.html', {"root": __dirname})
-// })
-// app.get('/About', function (req, res) {
-//   res.send('<h1>About us</h1')
-// })
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
-
+app.listen(app.get('port'), function() {
+    console.log('Expres server is running on port 3000!');
+});
